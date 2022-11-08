303823b14ad35007df59d125e1c7d75bf1b23a80
Lee, JoonYeong <joonyeonglee@gmail.com>
2022-11-08 13:58:18 +0900

    Commit 14

--- face_recognition/model_atari.py
+++ face_recognition/model_atari.py
@@ -1,4 +1,13 @@
 
+"""
+taken from: https://github.com/karpathy/minGPT/
+GPT model:
+- the initial stem consists of a combination of token encoding and a positional encoding
+- the meat of it is a uniform sequence of Transformer blocks
+    - each Transformer is a sequential combination of a 1-hidden-layer MLP block and a self-attention block
+    - all blocks feed into a central residual pathway similar to resnets
+- the final decoder is a linear projection into a vanilla Softmax classifier
+"""
 
 import math
 import logging
