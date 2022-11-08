f76e5c0d72e0de91c318bdf1bda410b0f0c03a7f
Lee, JoonYeong <joonyeonglee@gmail.com>
2022-11-08 13:59:07 +0900

    Commit 17

--- face_recognition/train.py
+++ face_recognition/train.py
@@ -18,7 +18,6 @@ import os
 
 if __name__ == '__main__':
 
-    #======= hyperparameters & data loaders =======#
     cfg = configurations[1]
 
     SEED = cfg['SEED'] # random seed for reproduce results
